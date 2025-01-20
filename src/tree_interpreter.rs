f533b46ab3df96acef8ca37104baf1b08ab9aeea
Meowu <474384902@qq.com>
2025-01-20 10:37:56 +0800

    eval binary.

--- src/tree_interpreter.rs
+++ src/tree_interpreter.rs
@@ -1,6 +1,7 @@
+use core::f64;
 use std::f64::NAN;
 
-use crate::expr::{self, Expr, Literal, UnaryOp, UnaryOpType};
+use crate::expr::{self, BinaryOp, Expr, Literal, UnaryOp, UnaryOpType};
 
 pub enum Value {
     Number(f64),
@@ -16,6 +17,7 @@ impl Interpreter {
         match expr {
             Expr::Literal(literal) => Ok(self.visit_literal(literal)),
             Expr::Unary(op, e) => self.visit_unary(*op, e),
+            Expr::Binary(lhs, op, rhs) => self.visit_binary(lhs, op.clone(), rhs),
             Expr::Grouping(e) => self.evaluate(e),
             _ => Err("E".to_string()),
         }
@@ -40,6 +42,48 @@ impl Interpreter {
         }
     }
 
+    fn visit_binary(&mut self, lhs: &Expr, op: BinaryOp, rhs: &Expr) -> Result<Value, String> {
+        let left = self.evaluate(lhs).unwrap();
+        let right = self.evaluate(rhs).unwrap();
+        match (&left, op.op_type, &right) {
+            (Value::Number(l), expr::BinaryOpType::Greater, Value::Number(r)) => {
+                Ok(Value::Boolean(l > r))
+            }
+            (Value::Number(l), expr::BinaryOpType::GreaterEqual, Value::Number(r)) => {
+                Ok(Value::Boolean(l >= r))
+            }
+            (Value::Number(l), expr::BinaryOpType::Less, Value::Number(r)) => {
+                Ok(Value::Boolean(l < r))
+            }
+            (Value::Number(l), expr::BinaryOpType::LessEqual, Value::Number(r)) => {
+                Ok(Value::Boolean(l <= r))
+            }
+            (Value::Number(l), expr::BinaryOpType::Plus, Value::Number(r)) => {
+                Ok(Value::Number(l + r))
+            }
+            (Value::Number(l), expr::BinaryOpType::Minus, Value::Number(r)) => {
+                Ok(Value::Number(l - r))
+            }
+            (Value::Number(l), expr::BinaryOpType::Star, Value::Number(r)) => {
+                Ok(Value::Number(l * r))
+            }
+            (Value::Number(ln), expr::BinaryOpType::Slash, Value::Number(rn)) => {
+                if *rn != 0.0 {
+                    Ok(Value::Number(ln / rn))
+                } else {
+                    Err("Division by zero.".to_string())
+                }
+            }
+            (Value::String(ls), expr::BinaryOpType::Plus, Value::String(rs)) => {
+                Ok(Value::String(String::from(ls) + &rs))
+            }
+            (_, expr::BinaryOpType::EqualEqual, _) => {
+                Ok(Value::Boolean(Self::equals(&left, &right)))
+            }
+            _ => Err("Invalid Operation".to_string()),
+        }
+    }
+
     fn is_truthy(val: &Value) -> bool {
         match val {
             Value::Nil => false,
@@ -51,5 +95,21 @@ impl Interpreter {
         // } else {
         //     false
         // }
+    }
+
+    fn equals(lhs: &Value, rhs: &Value) -> bool {
+        match (lhs, rhs) {
+            (Value::Boolean(b1), Value::Boolean(b2)) => b1 == b2,
+            (Value::String(s1), Value::String(s2)) => s1 == s2,
+            (Value::Number(n1), Value::Number(n2)) => {
+                if n1.is_nan() || n2.is_nan() {
+                    false
+                } else {
+                    (n1 - n2).abs() < f64::EPSILON
+                }
+            }
+            (Value::Nil, Value::Nil) => true,
+            _ => false,
+        }
     }
 }
